77d1f3fc27c5ef7b3a870a59fbb67417a4d85c12
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-11 09:46:45 +0100

    pass tempt and highlights data to components

---
+++ src/components/TempVarChart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="tempvarchart">
+    <p>Temperature Information:</p>
+    {{ tempVar }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tempVar"],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style></style>
